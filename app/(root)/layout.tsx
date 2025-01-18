5793c73fcb25bd356ed7a9ac45425da8ee6592f7
Samuel Ojagbohunmi <samm40me@yahoo.com>
2025-01-18 01:52:02 -0700

    second commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import SideBar from "@/components/SideBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Sam', lastName: 'Oja' };
+
   return (
-      <main>
-          SIDEBAR
+      <main className="flex h-screen w-full front-inter">
+      <SideBar user={loggedIn} />
           {children}
     </main>
   );
